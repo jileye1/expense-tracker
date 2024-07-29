720db01ddb55cf9698856c82abb311701da55b88
Josie Leye <jiley1@student.monash.edu>
2024-07-29 10:19:56 +1000

    Add Global Styles and Icons

--- frontend/src/utils/icons.js
+++ frontend/src/utils/icons.js
@@ -3,3 +3,5 @@ export const expenses = <i className="fa-solid fa-credit-card"></i>
 export const income = <i className="fa-solid fa-money-bill"></i>
 export const categories = <i className="fa-solid fa-tags"></i>
 export const budget = <i className="fa-solid fa-dollar-sign"></i>
+export const signout = <i className="fa-solid fa-right-from-bracket"></i>
+export const user = <i className="fa-solid fa-user"></i>
