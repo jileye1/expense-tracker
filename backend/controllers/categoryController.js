21ef14eb8690647ac60ae290ce512686a0025e72
Josie Leye <jiley1@student.monash.edu>
2024-07-30 21:50:43 +1000

    Fix category api value checking

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -2,7 +2,11 @@ const CategorySchema = require("../models/categoryModel");
 
 
 exports.addCategory = async (req, res) => {
-    const {name, budget_per_year, budget_per_month, budget_per_week} = req.body;
+    const { name, budget_per_year, budget_per_month, budget_per_week } = req.body;
+
+    console.log("year: " + budget_per_year)
+    console.log("month: " + budget_per_month)
+    console.log("week: " + budget_per_week)
 
     const category = CategorySchema({
         name,
@@ -11,18 +15,42 @@ exports.addCategory = async (req, res) => {
         budget_per_week
     });
 
+    console.log("cat-year: " + category.budget_per_year)
+    console.log("cat-month: " + category.budget_per_month)
+    console.log("cat-week: " + category.budget_per_week)
+
     try {
         // Validations
-        if(!name || !budget_per_year || !budget_per_month || !budget_per_week) {
-            return res.status(400).json({message: 'All fields required.'});
+        if (!name) {
+            return res.status(400).json({ message: 'Name required.' });
         }
-        if(budget_per_year <= 0 || budget_per_month <= 0 || budget_per_week <= 0) {
-            return res.status(400).json({message: 'Budget amount must be a positive number.'});
+        if (!budget_per_year) {
+            if (!budget_per_month) {
+                if (!budget_per_week) {
+                    return res.status(400).json({ message: 'One positive budget field required.' });
+                }
+                // Get budget from week
+                category.budget_per_year = (parseFloat(budget_per_week) / 7) * 365.25;
+                category.budget_per_month = category.budget_per_year / 12;
+            } else {
+                // get budget from month
+                category.budget_per_year = parseFloat(budget_per_month) * 12;
+                category.budget_per_week = (category.budget_per_year / 365.25) * 7;
+            }
+        } else {
+            // get budget from year
+            category.budget_per_month = parseFloat(budget_per_year) / 12;
+            category.budget_per_week = (parseFloat(budget_per_year) / 365.25) * 7;
         }
+
+        category.budget_per_year = category.budget_per_year.toFixed(2)
+        category.budget_per_month = category.budget_per_month.toFixed(2)
+        category.budget_per_week = category.budget_per_week.toFixed(2)
+
         await category.save();
         res.status(200).json(category);
     } catch (error) {
-        res.status(500).json({message: error});
+        res.status(500).json({ message: error });
     }
 
     console.log(category);
@@ -33,15 +61,15 @@ exports.getCategories = async (req, res) => {
         const categories = await CategorySchema.find();
         res.status(200).json(categories);
     } catch (error) {
-        res.status(500).json({message: error});
+        res.status(500).json({ message: error });
     }
 }
 
 exports.deleteCategory = async (req, res) => {
-    const {id} = req.params;
+    const { id } = req.params;
     CategorySchema.findByIdAndDelete(id).then((category) => {
         res.status(200).json(category);
     }).catch((error) => {
-        res.status(500).json({message: error});
+        res.status(500).json({ message: error });
     });
 }
