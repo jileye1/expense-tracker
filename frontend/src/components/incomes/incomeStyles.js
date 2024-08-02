238d289aca975566d5127f19ebc931251cef8417
Josie Leye <jiley1@student.monash.edu>
2024-08-02 17:00:43 +1000

    Make global input styles

--- frontend/src/components/incomes/incomeStyles.js
+++ frontend/src/components/incomes/incomeStyles.js
@@ -16,22 +16,6 @@ export const IncomeFormStyled = styled.form`
     display: flex;
     flex-direction: column;
     gap: 2rem;
-    input{
-        font-family: inherit;
-        font-size: inherit;
-        outline: none;
-        border: none;
-        padding: .5rem 1rem;
-        border-radius: 5px;
-        border: 2px solid #fff;
-        background: transparent;
-        resize: none;
-        box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
-        color: rgba(34, 34, 96, 0.9);
-        &::placeholder{
-            color: rgba(34, 34, 96, 0.4);
-        }
-    }
     .input-control{
         input{
             width: 100%;
