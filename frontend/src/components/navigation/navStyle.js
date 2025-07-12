aadac97fdf2a48794a0b7dcd2e4d733ad33757ea
Josie Leye <jiley1@student.monash.edu>
2025-07-12 12:12:55 +1000

    Fix Sign Out alignment

--- frontend/src/components/navigation/navStyle.js
+++ frontend/src/components/navigation/navStyle.js
@@ -58,6 +58,26 @@ gap: 2rem;
     }
 }
 
+.bottom-nav{
+    li{
+        display: grid;
+        grid-template-columns: 40px auto;
+        align-items: center;
+        margin: .6rem 0;
+        font-weight: 500;
+        cursor: pointer;
+        transition: all .4s ease-in-out;
+        color: rgba(34, 34, 96, .6);
+        padding-left: 1rem;
+        position: relative;
+        i{
+            color: rgba(34, 34, 96, 0.6);
+            font-size: 1.4rem;
+            transition: all .4s ease-in-out;
+        }
+    }
+}
+
 .active{
     color: rgba(34, 34, 96, 1) !important;
     i{
