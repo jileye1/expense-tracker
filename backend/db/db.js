998066626ea2bb8bc2d64a0bcff647330280bb78
Josie Leye <jiley1@student.monash.edu>
2025-07-29 17:10:09 +1000

    Create User model and update other models to include

--- backend/db/db.js
+++ backend/db/db.js
@@ -7,6 +7,7 @@ const db = async () => {
         console.log("DB Connected");
     } catch (error) {
         console.log("DB Connection Error");
+        console.log("Message: " + error);
     }
 }
 
---
+++ backend/models/userModel.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        maxLength: 100,
+        trim: true,
+        lowercase:true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 8
+    }
+}, {timestamps: true});
+
+// Hash password before saving
+UserSchema.pre('save', async function(next) {
+    if(!this.isModified('password')) return next();
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+// Method to compare password
+UserSchema.methods.comparePassword = async function(inputPassword) {
+    return await bcrypt.compare(inputPassword, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
