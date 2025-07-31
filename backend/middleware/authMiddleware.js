e8ade3573d7b3fe1ce3c3528390a799844b6e907
Josie Leye <jiley1@student.monash.edu>
2025-07-31 20:00:52 +1000

    Fix auth middleware bug

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -3,32 +3,27 @@ const UserSchema = require('../models/userModel');
 const { getJWTSecret } = require('../utils/config');
 
 const protect = async (req, res, next) => {
-    let token;
-
-    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-        try {
-            // Get token from header
-            token = req.headers.authorization.split(' ')[1];
-
-            // Verify token
-            const decoded = jwt.verify(token, getJWTSecret());
-
-            // Get user from the token
-            req.user = await UserSchema.findById(decoded.id).select('-password');
-
-            if (!req.user) {
-                return res.status(401).json({ message: 'User not found' });
-            }
-
-            next();
-        } catch (error) {
-            console.error(error);
-            res.status(401).json({ message: 'Authorisation failed' });
-        }
+    if (!req.headers.authorization || !req.headers.authorization.startsWith('Bearer')) {
+        return res.status(401).json({ message: 'Authorisation failed: no token' });
     }
 
-    if (!token) {
-        res.status(401).json({ message: 'Authorisation failed: no token' });
+    try {
+        // Get token from header
+        token = req.headers.authorization.split(' ')[1];
+
+        // Verify token
+        const decoded = jwt.verify(token, getJWTSecret());
+
+        // Get user from the token
+        req.user = await UserSchema.findById(decoded.id).select('-password');
+
+        if (!req.user) {
+            return res.status(401).json({ message: 'User not found' });
+        }
+
+        next();
+    } catch (error) {
+        return res.status(401).json({ message: 'Authorisation failed' });
     }
 };
 
